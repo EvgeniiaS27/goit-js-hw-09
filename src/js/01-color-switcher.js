fd24330d6bc425fc2d288f9c2c6509045a2521d2
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-06-11 13:36:39 +0300

    Task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtnRef.addEventListener('click', onButtonStartClick);
+stopBtnRef.addEventListener('click', onButtonStopClick);
+
+stopBtnRef.setAttribute('disabled', 'disabled');
+
+function onButtonStartClick() {
+  startBtnRef.setAttribute('disabled', 'disabled');
+  stopBtnRef.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onButtonStopClick() {
+  startBtnRef.removeAttribute('disabled');
+  stopBtnRef.setAttribute('disabled', 'disabled');
+  clearInterval(timerId);
+}
+
+// Генерация случайного цвета
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
